cca2bb025f3e74ffec50484e93c8d2807d57b9d2
ZOOU Qinn <qinn.zou@newbiiz.com>
2018-06-12 15:31:41 +0800

    处理Ma labs CAD商品数据。

---
+++ edi_product_newbiiz/README.rst
@@ -0,0 +1,10 @@
+Electronic Data Interchange for Products
+========================================
+
+EDI capability for the Odoo ```product``` module.
+
+Key Features
+------------
+* Quickly import product lists from external EDI sources
+* Automatic deduplication of unmodified product records
+* Easily customisable to handle new or custom document formats
---
+++ edi_product_newbiiz/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': "EDI for Products",
+    'description': "",
+    'version': '0.1',
+    'depends': ['edi', 'product_newbiiz'],
+    'website': "http://www.newbiiz.com",
+    'author': "ZOOU Qinn <qinn.zou@newbiiz.com>",
+    'category': "Extra Tools",
+    'data': [
+        'views/edi_product_views.xml',
+        'views/edi_product_malabscad_views.xml',
+        'data/edi_product_data.xml',
+        'data/edi_product_malabscad_data.xml',
+    ],
+}
