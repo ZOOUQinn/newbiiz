285d85116a3e2edf6e82f57f6b7eeca3228265e2
ZOOU Qinn <qinn.zou@newbiiz.com>
2018-07-21 15:58:20 +0800

    sale_newbiiz report for sale.order

---
+++ sale_newbiiz/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Sale Newbiiz',
+    'version': '1.0',
+    'category': 'Extra Tools',
+    'description': '',
+    'author': 'ZOOU Qinn <qinn.zou@newbiiz.com>',
+    'depends': [
+        'sale_management',
+        'product_newbiiz',
+    ],
+    'data': [
+        'reports/report.xml',
+    ],
+    'website': "http://www.newbiiz.com",
+}
