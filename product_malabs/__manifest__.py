e4051899b7ac8314f0336c1cca9022c12bac9286
ZOOU Qinn <qinn.zou@newbiiz.com>
2018-12-18 13:43:12 +0800

    Create price list for instant rebate.

--- product_malabs/__manifest__.py
+++ product_malabs/__manifest__.py
@@ -5,6 +5,7 @@
     'depends': ['product', 'sale_stock',],
     'data': [
         'data/company.xml',
+        'data/pricelist.xml',
         'views/view_from_studio.xml',
     ],
     'website': "http://www.newbiiz.com",
